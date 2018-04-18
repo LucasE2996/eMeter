d7108392aed64a787a8273d0fc77d5c2a23c279a
Lucas Rosa <moeda.60@gmail.com>
2018-04-18 13:03:09 -0300

    created controllers

---
+++ src/main/java/emonitor/app/restapi/MeterController.java
@@ -0,0 +1,4 @@
+package emonitor.app.restapi;
+
+public class MeterController {
+}
---
+++ src/main/java/emonitor/app/restapi/UserController.java
@@ -0,0 +1,7 @@
+package emonitor.app.restapi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
